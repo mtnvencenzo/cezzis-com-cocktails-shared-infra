9fe27fb9aaf2835dbebca6323e29a54b2752c962
mtnvencenzo <rvecchi@gmail.com>
2026-04-01 20:09:32 -0600

    updates

--- .iac/terraform/datasources.tf
+++ .iac/terraform/datasources.tf
@@ -23,15 +23,16 @@ data "azurerm_subnet" "cae_subnet" {
   resource_group_name  = "rg-${var.sub}-${var.region}-${var.global_environment}-network-${var.sequence}"
 }
 
-# data "azurerm_cdn_frontdoor_profile" "global_shared_cdn" {
-#   name                = "afd-${var.sub}-${var.region}-${var.global_environment}-shared-${var.sequence}"
-#   resource_group_name = data.azurerm_resource_group.global_shared_resource_group.name
-# }
+data "azurerm_cdn_frontdoor_profile" "global_shared_cdn" {
+  name                = "afd-${var.sub}-${var.region}-${var.global_environment}-shared-${var.sequence}"
+  resource_group_name = data.azurerm_resource_group.global_shared_resource_group.name
+}
 
-# data "azurerm_dns_zone" "cezzis_dns_zone" {
-#   name                = "cezzis.com"
-#   resource_group_name = data.azurerm_resource_group.cocktails_global_resource_group.name
-# }
+data "azurerm_cdn_frontdoor_endpoint" "global_shared_cdn_endpoint" {
+  name                = "afde-${var.sub}-${var.region}-${var.global_environment}-shared-${var.sequence}"
+  profile_name        = data.azurerm_cdn_frontdoor_profile.global_shared_cdn.name
+  resource_group_name = data.azurerm_resource_group.global_shared_resource_group.name
+}
 
 data "azurerm_key_vault" "global_keyvault" {
   name                = "kv-${var.sub}-${var.region}-${var.global_environment}-shared-${var.short_sequence}"
