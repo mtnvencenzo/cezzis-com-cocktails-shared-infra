89990806c4a7f04bd1043260551fcffcaf322e74
mtnvencenzo <rvecchi@gmail.com>
2026-04-02 12:36:53 -0600

    feat(topics): adding cocktail sb topics

---
+++ .iac/terraform/servicebus.tf
@@ -0,0 +1,56 @@
+module "servicebus_account_updates_topic" {
+  source = "git::ssh://git@github.com/mtnvencenzo/Terraform-Modules.git//modules/servicebus-topic"
+
+  namespace_id        = data.azurerm_servicebus_namespace.servicebus_namespace.id
+  name_discriminator  = "account-updates"
+  sub                 = var.sub
+  region              = var.region
+  environment         = var.environment
+  domain              = var.domain
+  resource_group_name = data.azurerm_resource_group.cocktails_resource_group.name
+  location            = data.azurerm_resource_group.cocktails_resource_group.location
+
+  providers = {
+    azurerm = azurerm
+  }
+
+  tags = local.tags
+}
+
+module "servicebus_cocktail_updates_topic" {
+  source = "git::ssh://git@github.com/mtnvencenzo/Terraform-Modules.git//modules/servicebus-topic"
+
+  namespace_id        = data.azurerm_servicebus_namespace.servicebus_namespace.id
+  name_discriminator  = "cocktail-updates"
+  sub                 = var.sub
+  region              = var.region
+  environment         = var.environment
+  domain              = var.domain
+  resource_group_name = data.azurerm_resource_group.cocktails_resource_group.name
+  location            = data.azurerm_resource_group.cocktails_resource_group.location
+
+  providers = {
+    azurerm = azurerm
+  }
+
+  tags = local.tags
+}
+
+module "servicebus_cocktail_user_actions_topic" {
+  source = "git::ssh://git@github.com/mtnvencenzo/Terraform-Modules.git//modules/servicebus-topic"
+
+  namespace_id        = data.azurerm_servicebus_namespace.servicebus_namespace.id
+  name_discriminator  = "cocktail-user-actions"
+  sub                 = var.sub
+  region              = var.region
+  environment         = var.environment
+  domain              = var.domain
+  resource_group_name = data.azurerm_resource_group.cocktails_resource_group.name
+  location            = data.azurerm_resource_group.cocktails_resource_group.location
+
+  providers = {
+    azurerm = azurerm
+  }
+
+  tags = local.tags
+}
